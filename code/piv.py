d55d2cacb4e17287fca01758d71d883153c64e01
nealbob <hughes.neal@gmail.com>
2026-04-24 09:36:48 +1000

    s

--- code/piv.py
+++ code/piv.py
@@ -5,5 +5,19 @@ mydata = mydata.reset_index()
 mydata.loc[mydata.columnA > 0, "columnC"] = mydata["columnB"] / mydata["columnA"]
 
 
+mydata = (mydata                                                                                                                                                                                                       
+.groupby('year')                                                                                                                                                                                                     
+[["columnA", "columnB", "columnC"]]                                                                                                                                                                                  
+.mean()                                                                                                                                                                                                              
+.reset_index()                                                
+)
+
+mask = mydata.columnA > 0
+mydata.loc[mask, "columnC"] = (
+mydata["columnB"]
+/ mydata["columnA"]
+)
+
+
 
 
